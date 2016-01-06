367c1313e244bf0d34c788e29c8f322580af1bbd
Jacob Wright <jacwright@gmail.com>
2016-01-06 13:25:55 -0700

    Fixes bug in slide-move animation

--- animations/index.js
+++ animations/index.js
@@ -7,8 +7,8 @@ module.exports = function(fragments) {
   }
 
   fragments.registerAnimation('fade', require('./fade')());
-  fragments.registerAnimation('slide-h', require('./slide-horizontal')());
-  fragments.registerAnimation('slide-move-h', require('./slide-move-horizontal')());
-  fragments.registerAnimation('slide-move', require('./slide-move')());
   fragments.registerAnimation('slide', require('./slide')());
+  fragments.registerAnimation('slide-h', require('./slide-horizontal')());
+  fragments.registerAnimation('slide-move', require('./slide-move')(fragments));
+  fragments.registerAnimation('slide-move-h', require('./slide-move-horizontal')(fragments));
 };
--- animations/slide-move-horizontal.js
+++ animations/slide-move-horizontal.js
@@ -1,8 +1,8 @@
 /**
  * Move items left and right in a list
  */
-module.exports = function(options) {
+module.exports = function(fragments, options) {
   if (!options) options = {};
   if (!options.property) options.property = 'width';
-  return require('./slide-move')(options);
+  return require('./slide-move')(fragments, options);
 };
