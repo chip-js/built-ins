9376ca6af98c21deac636870054d68406456eda9
Jacob Wright <jacwright@gmail.com>
2016-01-19 09:39:55 -0700

    Log will log out any changes

--- binders/log.js
+++ binders/log.js
@@ -4,6 +4,12 @@
 module.exports = function() {
   return {
     priority: 60,
+    created: function() {
+      if (this.observer) {
+        this.observer.getChangeRecords = true;
+      }
+    },
+
     updated: function(value) {
       /*eslint-disable no-console */
       console.log('%cDebug: %c' + this.expression, 'color:blue;font-weight:bold', 'color:#DF8138', '=', value);
