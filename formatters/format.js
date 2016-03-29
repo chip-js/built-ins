71fbc7e812edd283da4820001ee2adec564ffcb1
Jacob Wright <jacob.wright@riotgames.com>
2016-03-29 14:20:14 -0600

    Fix scope for `format` formatter

--- formatters/format.js
+++ formatters/format.js
@@ -2,5 +2,5 @@
  * Formats the value something returned by a formatting function passed. Use for custom or one-off formats.
  */
 module.exports = function(value, formatter, isSetter) {
-  return formatter(value, isSetter);
+  return formatter.call(this, value, isSetter);
 };
