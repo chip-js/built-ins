7578fc0d58143c5b706ac3267343e3fcc45ff6fc
Jacob Wright <jacob.wright@riotgames.com>
2016-04-11 08:11:50 -0600

    Update to use `get` in binding

--- binders/radio.js
+++ binders/radio.js
@@ -7,40 +7,30 @@ module.exports = function(valueName) {
     priority: 10,
 
     compiled: function() {
-      var valueExpr;
       var element = this.element;
 
       if (valueName && valueName !== 'value' && element.hasAttribute(valueName)) {
-        valueExpr = this.fragments.codifyExpression('attribute', element.getAttribute(valueName), true);
+        this.valueExpr = this.fragments.codifyExpression('attribute', element.getAttribute(valueName), true);
         element.removeAttribute(valueName);
       } else if (element.hasAttribute('value')) {
-        valueExpr = this.fragments.codifyExpression('attribute', element.getAttribute('value'), false);
+        this.valueExpr = this.fragments.codifyExpression('attribute', element.getAttribute('value'), false);
       } else {
         return false;
       }
 
       element.setAttribute('name', this.expression);
-      this.valueObserver = this.observe(valueExpr);
     },
 
     created: function() {
       this.element.addEventListener('change', function(event) {
         if (this.element.checked) {
-          this.observer.set(this.valueObserver.get());
+          this.observer.set(this.get(this.valueExpr));
         }
       }.bind(this));
     },
 
-    bound: function() {
-      this.valueObserver.bind(this.context);
-    },
-
-    unbound: function() {
-      this.valueObserver.unbind();
-    },
-
     updated: function(value) {
-      this.element.checked = (value == this.valueObserver.get());
+      this.element.checked = (value == this.get(this.valueExpr));
     }
   };
 };
